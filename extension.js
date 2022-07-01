cc8c8f471fcadd2fe2e3498f00aeec4bde51d444
Anutas <92079514+Anutas@users.noreply.github.com>
2022-07-01 13:31:03 +0700

    Initial commit

---
+++ extension.js
@@ -0,0 +1,12 @@
+({
+    name: "DISTANCE", // Category Name
+    description: "ZX-Distance Sensor",
+    author: "Aphibun",
+    category: "Sensors",
+    version: "1.0.0",
+    icon: "/static/icon.png", // Category icon
+    color: "#27AE60", // Category color (recommend some blocks color)
+    blocks: [ // Blocks in Category
+        "distance_read"
+    ]
+});
